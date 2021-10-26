3c67d436a0db0c251a8296f6dc4d41d5e5d8487a
juninhoww2 <alqadianoKira@gmail.com>
2021-10-25 21:59:58 -0300

    [RESTful APIs in 100 Second]

---
+++ index.js
@@ -0,0 +1,22 @@
+const app = require('express')();
+const PORT = 8888;
+
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: '🥋', 
+        size: 'large'
+    })
+});
+
+app.post('/tshirt/:id', (req, res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        res.status(418).send({ message: 'We need a logo!' })
+    }
+
+    res.send({
+        tshirt: `🥋 with your ${logo} and ID of ${id}``,  
+    })
+}) 
